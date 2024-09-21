982e88f8b385a35565c96b9aaf182f54eb5cd009
AJRMRODMAR <antoniodejesusrodriguez928@gmail.com>
2024-09-20 18:07:21 -0600

    SEO 1

---
+++ assets/js/contacto.js
@@ -0,0 +1,41 @@
+document
+  .getElementById("button-whatssapp")
+  .addEventListener("click", function () {
+    window.open("https://wa.me/+524612668518", "_blank", "noopener,noreferrer");
+  });
+
+document
+  .getElementById("youtube-button")
+  .addEventListener("click", function () {
+    window.open(
+      "https://www.youtube.com/@IMarketCelaya",
+      "_blank",
+      "noopener,noreferrer"
+    );
+  });
+document.getElementById("button-correo").addEventListener("click", function () {
+  window.open(
+    "mailto:imarketingandpublicity@outlook.com",
+    "_blank",
+    "noopener,noreferrer"
+  );
+});
+
+document
+  .getElementById("button-instagram")
+  .addEventListener("click", function () {
+    window.open(
+      "https://instagram.com/ingeniosmex?igshid=MzRlODBiNWFlZA==",
+      "_blank",
+      "noopener,noreferrer"
+    );
+  });
+document
+  .getElementById("button-facebook")
+  .addEventListener("click", function () {
+    window.open(
+      "https://www.facebook.com/profile.php?id=100088862331341&mibextid=V3Yony",
+      "_blank",
+      "noopener,noreferrer"
+    );
+  });
