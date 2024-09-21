fc77e00fc6264b4a79f56a3afc55632a50894a47
AJRMRODMAR <antoniodejesusrodriguez928@gmail.com>
2024-09-20 18:09:50 -0600

    CORREO

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -8,14 +8,14 @@ document
   .getElementById("youtube-button")
   .addEventListener("click", function () {
     window.open(
-      "https://www.youtube.com/@IMarketCelaya",
+      "",
       "_blank",
       "noopener,noreferrer"
     );
   });
 document.getElementById("button-correo").addEventListener("click", function () {
   window.open(
-    "mailto:imarketingandpublicity@outlook.com",
+    "mailto:mantistecnologia077@gmail.com",
     "_blank",
     "noopener,noreferrer"
   );
@@ -25,7 +25,7 @@ document
   .getElementById("button-instagram")
   .addEventListener("click", function () {
     window.open(
-      "https://instagram.com/ingeniosmex?igshid=MzRlODBiNWFlZA==",
+      " ",
       "_blank",
       "noopener,noreferrer"
     );
@@ -34,7 +34,7 @@ document
   .getElementById("button-facebook")
   .addEventListener("click", function () {
     window.open(
-      "https://www.facebook.com/profile.php?id=100088862331341&mibextid=V3Yony",
+      " ",
       "_blank",
       "noopener,noreferrer"
     );
